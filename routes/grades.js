4c2b27bc410f695404736b63a28cc5e087074bc1
goolsbyash <goolsbyashley015@gmail.com>
2024-01-18 14:23:40 -0500

    Added Mongoose methods to GET route for /student/:id

--- routes/grades.js
+++ routes/grades.js
@@ -24,12 +24,22 @@ router.get("/:id", async (req, res) => {
 
 // Get a student's grade data
 router.get("/student/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { student_id: Number(req.params.id) };
-  let result = await collection.find(query).toArray();
+  //   let collection = await db.collection("grades");
+  //   let query = { student_id: Number(req.params.id) };
+  //   let result = await collection.find(query).toArray();
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+  //   if (!result) res.send("Not found").status(404);
+  //   else res.send(result).status(200);
+  try {
+    // Includes scores of all objects with matching learner id
+    let result = await Grade.find({ learner_id: req.params.id }).select(
+      "scores"
+    );
+    console.log(result.length);
+    res.send(result);
+  } catch {
+    res.status(404).send("Not Found");
+  }
 });
 
 // Get a class's grade data
