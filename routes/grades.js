d351ebe7866cf304bfb7b39b1b22be7b1e90b44d
goolsbyash <goolsbyashley015@gmail.com>
2024-01-18 14:03:54 -0500

    Adjusted GET route for /:id to use Mongoose methods.

--- routes/grades.js
+++ routes/grades.js
@@ -1,17 +1,25 @@
 import express from "express";
-import db from "../db/conn.js";
-import { ObjectId } from "mongodb";
+// import db from "../db/conn.js";
+// import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import Grade from "../models/grades.js";
 
 const router = express.Router();
 
 // Get a single grade entry
 router.get("/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
-  let result = await collection.findOne(query);
+  //   let collection = await db.collection("grades");
+  //   let query = { _id: ObjectId(req.params.id) };
+  //   let result = await collection.findOne(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+  //   if (!result) res.send("Not found").status(404);
+  //   else res.send(result).status(200);
+  try {
+    let result = await Grade.find({ learner_id: req.params.id });
+    res.send(result);
+  } catch {
+    res.status(404).send("Not Found");
+  }
 });
 
 // Get a student's grade data
